97f872f8126c597d72eadfb4df3215ce543f34db
Taito <13.taito@gmail.com>
2022-01-30 09:15:33 +0900

    修正しました

--- Kuku.java
+++ Kuku.java
@@ -3,9 +3,9 @@ public class Kuku {
 	public static void main(String[] args){
 		for(int i = 1; i < 10; i++){
 			for(int j = 1; j < 10; j++){
-				System.out.printf(" %2d", i * j);
+				System.out.println(" %2d", i * j);
 			}
-			System.out.println();
+			System.out.println("終了しました");
 		}
 	}
 }
